416a3bbc19b0f8b5497e71263a3ccec3c3485df8
Hitesh Patel <hitesh@pixelhexdigital.com>
2024-01-17 19:36:42 +0530

    fix(server): gstn controller

--- src/controllers/apps/verify-gstn/gstn.controllers.js
+++ src/controllers/apps/verify-gstn/gstn.controllers.js
@@ -1,3 +1,5 @@
+import { ApiError } from "../../../utils/ApiError.js";
+import { ApiResponse } from "../../../utils/ApiResponse.js";
 import { asyncHandler } from "../../../utils/asyncHandler.js";
 
 import axios from "axios";
@@ -39,10 +41,12 @@ const verifyGSTNController = asyncHandler(async (req, res) => {
       yearOfEstablishment: gtiFY || "",
     };
 
-    return res.status(200).json(result);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, result, "gst number verified"));
   } catch (error) {
     console.error("Error fetching GST data:", error.message);
-    return res.status(500).json({ error: "Internal Server Error" });
+    throw new ApiError(422, `invalid gst Number`);
   }
 });
 
